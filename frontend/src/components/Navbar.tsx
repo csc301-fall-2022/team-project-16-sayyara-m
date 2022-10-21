a17e748f2f910745ac16ecf1e29c72a537049f36
Hamoon Zamiri <hamoon.zamiri@mail.utoronto.ca>
2022-10-20 23:00:43 -0400

    restyled navbar

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -3,13 +3,13 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
     return (
-        <div className="flex items-center bg-slate-400 justify-between w-full h-24 p-4">
+        <div className="flex items-center bg-gray-100 justify-between w-full h-24 p-4">
             <div className='text-5xl font-semibold text-blue-800'>
                 Sayyara  {/* We could put a logo here instead */}
             </div>
-            <div className="flex ">
-                <Link to="/">Login</Link>
-                <Link to="/SignUp">Sign Up</Link>
+            <div className="mx-2">
+                <Link className="mx-2" to="/">Login</Link>
+                <Link className="mx-2" to="/SignUp">Sign Up</Link>
             </div>
         </div>
     )
