f32c7864afba2aceb76438d7f15c428a4b057559
Hamoon <90403097+HamoonZamiri@users.noreply.github.com>
2022-10-20 23:51:16 -0400

    Homepage (#9)
    * created basic navbar component and home page, rendered navbar in homepage and added home to routes
    
    * restyled navbar
    
    * added link to homepage on sayyara logo in login page
    
    * attached home page link to login button instead of Sayyara logo in Login.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
+import Home from './pages/Home';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
 import Login from './pages/Login';
 import SignUp from './pages/SignUp';
 
 function App() {
   return (
+
     <Router>
       <Routes>
         <Route path='/' element={<Login/>} />
         <Route path='/SignUp' element={<SignUp/>} />
+        <Route path='/home' element={<Home/>} />
       </Routes>
     </Router>
     );
--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -9,15 +9,15 @@ function Login() {
 
     const renderDropdown = () => {
         // Renders the dropdown menu depending on the visibility state.
-        
+
         if (!menuVisible) {
             return (<></>);
         }
         return(
-            <div className="absolute right-0 z-10 mt-1 w-full origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 
+            <div className="absolute right-0 z-10 mt-1 w-full origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5
             focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="menu-button" tabIndex={-1}>
                 <div className="py-1" role="none">
-                    <a href="#" className="text-gray-700 hover:text-gray-900 hover:bg-gray-100 block px-4 py-2 text-sm" role="menuitem" 
+                    <a href="#" className="text-gray-700 hover:text-gray-900 hover:bg-gray-100 block px-4 py-2 text-sm" role="menuitem"
                     tabIndex={-1} id="menu-item-0"
                     onClick={() => {
                         setLoginUserType("Vehicle Owner");
@@ -25,7 +25,7 @@ function Login() {
                     }}>
                         Vehicle Owner
                     </a>
-                    <a href="#" className="text-gray-700 hover:text-gray-900 hover:bg-gray-100 block px-4 py-2 text-sm" role="menuitem" 
+                    <a href="#" className="text-gray-700 hover:text-gray-900 hover:bg-gray-100 block px-4 py-2 text-sm" role="menuitem"
                     tabIndex={-1} id="menu-item-1"
                     onClick={() => {
                         setLoginUserType("Shop Owner");
@@ -43,7 +43,7 @@ function Login() {
             <div className='flex flex-wrap h-full max-w-md min-w-[330px] w-full'>
                 <div className='flex w-full justify-center relative'>
                     <div className='absolute bottom-32 text-7xl font-semibold text-blue-800'>
-                        Sayyara  {/* We could put a logo here instead */} 
+                        Sayyara  {/* We could put a logo here instead */}
                     </div>
                 </div>
                 <div className='w-full h-min border-2 border-gray-300 rounded-lg shadow-lg bg-white'>
@@ -55,15 +55,15 @@ function Login() {
                             {/* <span className='mr-1 font-bold'>As: </span> */}
                             <div className="relative inline-block text-left">
                                 <div>
-                                    <button type="button" className="inline-flex w-full justify-center rounded-md border 
-                                    border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 shadow-sm hover:bg-gray-50" 
+                                    <button type="button" className="inline-flex w-full justify-center rounded-md border
+                                    border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 shadow-sm hover:bg-gray-50"
                                     id="menu-button" aria-expanded="true" aria-haspopup="true"
                                     onClick={() => { setMenuVisible(!menuVisible) }}>
                                         {loginUserType}
                                         {/* Import - Heroicon name: mini/chevron-down */}
-                                        <svg className="-mr-1 ml-2 h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" 
+                                        <svg className="-mr-1 ml-2 h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"
                                         fill="currentColor" aria-hidden="true">
-                                            <path fillRule="evenodd" d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 
+                                            <path fillRule="evenodd" d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75
                                             0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z" clipRule="evenodd" />
                                         </svg>
                                     </button>
@@ -75,25 +75,25 @@ function Login() {
                             <label className="block text-gray-700 font-bold mb-2">
                                 Email
                             </label>
-                            <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight 
+                            <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight
                             focus:outline-blue-500 focus:shadow-outline" id="email" type="text" placeholder="Email Address"/>
                         </div>
                         <div className="mb-3">
                             <label className="block text-gray-700 font-bold mb-2">
                                 Password
                             </label>
-                            <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight 
+                            <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight
                             focus:outline-blue-500 focus:shadow-outline" id="password" type="password" placeholder="********"/>
                             <p className="text-red-500 text-xs italic" hidden={true}>
                                 Please choose a password.
                             </p>
                         </div>
                         <div className="flex items-center justify-between mb-6">
-                            <button className="transition duration-100 ease-in-out w-32 bg-blue-500 hover:bg-blue-700 text-white 
+                            <button className="transition duration-100 ease-in-out w-32 bg-blue-500 hover:bg-blue-700 text-white
                             font-semibold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
-                                Log In
+                                <Link to="/home">Log In</Link>
                             </button>
-                            <a className="transition duration-100 ease-in-out inline-block align-baseline font-bold text-sm 
+                            <a className="transition duration-100 ease-in-out inline-block align-baseline font-bold text-sm
                             text-blue-500 hover:text-blue-800" href="#">
                                 Forgot Password?
                             </a>
