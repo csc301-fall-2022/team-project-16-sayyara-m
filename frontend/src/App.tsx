cbcd0d16b7391fc00319575fbce41c72d4afa526
Nathan Raymant <nathan.raymant@mail.utoronto.ca>
2022-10-20 22:54:42 -0400

    Changed HashRouter to BrowserRouter

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './pages/Login';
 import SignUp from './pages/SignUp';
 
