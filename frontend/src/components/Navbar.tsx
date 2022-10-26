499b72f9d715462af0b7826ed5753bb870861f76
Uthman Mohamed <uthman.mohamed@mail.utoronto.ca>
2022-10-26 14:38:44 -0400

    Fix navbar mobile ui to render dropdown

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,22 +1,23 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import { Disclosure } from "@headlessui/react"
+import React, {useState} from "react";
+import {Link} from "react-router-dom";
+import {Disclosure} from "@headlessui/react"
 import ProfileDropdown from "./ProfileDropdown";
+import MenuIcon from '@mui/icons-material/Menu';
+
 //hidden on web version
 //sm:hidden on mobile version
 //seperate react code
 const navigation = [
-    { name: "Home", to: "/home", current: true },
-    { name: "Appointments", to: "/appointments", current: false },
+    {name: "Home", to: "/home", current: true},
+    {name: "Appointments", to: "/appointments", current: false},
 ];
 
 const MobileNavMap = () => {
     return (
-        <Disclosure.Panel className="sm:hidden flex flex-col sm:flex-row text-white sm:text-black \
-            bg-slate-500 sm:bg-inherit sm:ml-10 text-sm sm:text-lg md:text-xl items-start">
+        <Disclosure.Panel className="sm:hidden flex flex-col text-sm items-start">
             {navigation.map(item => (
-                <Disclosure.Button className="transition duration-100 ease-in-out  bg-gray-10 hover:bg-blue-500 \
-            font-semibold py-2 px-4 rounded focus:outline-none focus:shadow-outline ml-2" type="button">
+                <Disclosure.Button className="transition duration-100 ease-in-out  bg-gray-10 hover:bg-blue-300 \
+            font-semibold py-2 px-4 rounded focus:outline-none focus:shadow-outline w-full text-left" type="button">
                     <Link to={item.to}>{item.name}</Link>
                 </Disclosure.Button>
             ))}
@@ -37,24 +38,35 @@ const WebNavMap = () => {
     )
 };
 const Navbar = () => {
+
+    const [user, _] = useState(null);
+
     return (
-        <Disclosure as="nav" className="flex flex-col sm:flex-row items-center bg-gray-100 w-full px-4 sm:px-6 \
-        pt-6 pb-12 md:px-8">
-            <Disclosure.Button className="sm:hidden">Dash</Disclosure.Button>
-            <div className='text-xl sm:pr-6 sm:text-3xl md:text-5xl font-semibold text-blue-800 self-center sm:justify-start'>
-                Sayyara  {/* We could put a logo here instead */}
+        <Disclosure as="nav" className="bg-gray-200 w-full px-4 sm:px-6 \
+        pt-6 pb-5 md:px-8">
+            <div className="flex justify-between">
+                <div className="w-2/5 pl-0 sm:hidden">
+                    <Disclosure.Button className="text-left ml-4 sm:hidden active:bg-inherit">
+                        <MenuIcon />
+                    </Disclosure.Button>
+                </div>
+                <div
+                    className='text-xl sm:pr-6 sm:text-3xl md:text-5xl font-semibold text-blue-800 self-center sm:justify-start'>
+                    Sayyara {/* We could put a logo here instead */}
+                </div>
+
+                {/* hidden by default all styles applied with sm: */}
+                <WebNavMap/>
+
+                <div className="flex w-2/5 text-right justify-end items-center text-sm sm:ml-auto sm:text-xl">
+                    {user && <ProfileDropdown/>}
+                    <Link className="mx-2" to="/">Login</Link>
+                    <Link className="mx-2 text-ellipsis" to="/SignUp">Sign Up</Link>
+                </div>
             </div>
+
             {/* sm:hidden applied, hidden after 640px */}
-            <MobileNavMap />
-
-            {/* hidden by default all styles applied with sm: */}
-            <WebNavMap />
-
-            <div className="flex text-sm sm:ml-auto sm:text-xl">
-                <ProfileDropdown />
-                <Link className="mx-2" to="/">Login</Link>
-                <Link className="mx-2 text-ellipsis" to="/SignUp">Sign Up</Link>
-            </div>
+            <MobileNavMap/>
         </Disclosure>
     )
 }
