880171fe0bf3f93c1d91b7a16da16c6a93d928d4
haolin <haolinf@gmail.com>
2022-10-31 16:00:10 -0400

    Implemented QuoteService and QuoteController

--- backend/src/main/java/com/backend/spring/appointment/AppointmentService.java
+++ backend/src/main/java/com/backend/spring/appointment/AppointmentService.java
@@ -6,7 +6,6 @@ import org.springframework.stereotype.Service;
 import javax.transaction.Transactional;
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Optional;
 
 @Service
 public class AppointmentService {
