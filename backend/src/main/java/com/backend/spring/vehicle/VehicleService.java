04eba7cec410e68181a0f60cc1e991efdf319505
haolin <haolinf@gmail.com>
2022-10-31 17:24:36 -0400

    Created Vehicle package; implemented VehicleRepository, VehicleService, and VehicleController

---
+++ backend/src/main/java/com/backend/spring/vehicle/VehicleService.java
@@ -0,0 +1,63 @@
+package com.backend.spring.vehicle;
+
+import com.backend.spring.user.vehicleowner.VehicleOwner;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.List;
+
+@Service
+public class VehicleService {
+    private final VehicleRepository repository;
+
+    @Autowired
+    public VehicleService(VehicleRepository repository) {
+        this.repository = repository;
+    }
+
+    public List<Vehicle> getAllVehicles() {
+        return repository.findAll();
+    }
+
+    public Vehicle getVehicle(long id) {
+        return repository.findById(id).orElseThrow(IllegalStateException::new);
+    }
+
+    public Vehicle createVehicle(Vehicle vehicle) {
+        return repository.save(vehicle);
+    }
+
+    public void deleteVehicle(long id) {
+        repository.deleteById(id);
+    }
+
+    @Transactional
+    public void updateVehicle(long id, VehicleOwner vehicleOwner, Integer year, String make, String model, String vin, String plate) {
+        Vehicle vehicle = repository.findById(id).orElseThrow(IllegalStateException::new);
+
+        if (vehicleOwner != null) {
+            vehicle.setOwner(vehicleOwner);
+        }
+
+        if (year != null) {
+            vehicle.setYear(year);
+        }
+
+        if (make != null) {
+            vehicle.setMake(make);
+        }
+
+        if (model != null) {
+            vehicle.setModel(model);
+        }
+
+        if (vin != null) {
+            vehicle.setVin(vin);
+        }
+
+        if (plate != null) {
+            vehicle.setPlate(plate);
+        }
+    }
+}
