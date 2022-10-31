8aabfd938ad06e9af0a8a8b8331ccae1fc03e25e
haolin <haolinf@gmail.com>
2022-10-31 15:32:29 -0400

    Implemented AppointmentService

---
+++ backend/src/main/java/com/backend/spring/appointment/AppointmentService.java
@@ -0,0 +1,48 @@
+package com.backend.spring.appointment;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class AppointmentService {
+    private final AppointmentRepository repository;
+
+    @Autowired
+    public AppointmentService(AppointmentRepository repository) {
+        this.repository = repository;
+    }
+
+    public List<Appointment> getAllAppointments() {
+        return repository.findAll();
+    }
+
+    public Optional<Appointment> getAppointment(long id) {
+        return repository.findById(id);
+    }
+
+    public Appointment createAppointment(Appointment appointment) {
+        return repository.save(appointment);
+    }
+
+    public void deleteAppointment(long id) {
+        repository.deleteById(id);
+    }
+
+    @Transactional
+    public void updateAppointment(long id, LocalDateTime startDate, LocalDateTime endDate) {
+        Appointment appointment = repository.findById(id).orElseThrow(IllegalStateException::new);
+
+        if (startDate != null) {
+            appointment.setStartDate(startDate);
+        }
+
+        if (endDate != null) {
+            appointment.setStartDate(endDate);
+        }
+    }
+}
