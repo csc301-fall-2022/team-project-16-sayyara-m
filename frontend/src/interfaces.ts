34254472b59c9153edb1fe83b324cbe65b374ba4
Hamoon Zamiri <hamoon.zamiri@mail.utoronto.ca>
2022-11-01 12:42:46 -0400

    created MyShop page and created interfaces that map to java entities

---
+++ frontend/src/interfaces.ts
@@ -0,0 +1,55 @@
+export interface User {
+    id: number,
+    firstName: string,
+    lastName: string,
+    userName: string,
+    phoneNumber: string
+    email: string,
+}
+
+interface Address {
+    addressId: number,
+    street: string,
+    streetNumber: string,
+    postalCode: string,
+    province: string
+}
+
+interface Shop {
+    shopId: number,
+    shopName: string,
+    address: Address,
+    email?: string,
+    appointments: Appointment[]
+    quotes: Quote[]
+}
+
+export interface ShopOwner extends User {
+    shop: Shop
+}
+interface Vehicle {
+    year: number,
+    make: string,
+    model: string,
+    vin: string,
+    plate: string
+}
+
+export interface VehicleOwner extends User {
+    vehicle: Vehicle
+}
+
+export interface Appointment {
+    appointmentId: string,
+    vehicleOwner: VehicleOwner,
+    startTime: Date,
+    endTime: Date,
+    day: string,
+    duration: number
+}
+
+export interface Quote {
+    vehicleOwner: VehicleOwner,
+    price: number,
+    expiryTime: string
+}
---
+++ frontend/src/pages/MyShop.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const MyShop = () => {
+    return (
+        <div>
+            <div>
+                My Shop
+            </div>
+            <div>
+                <h1>My Appointments</h1>
+            </div>
+            <div>
+                <h1>My Quotes</h1>
+            </div>
+        </div>
+    )
+};
+export default MyShop;
