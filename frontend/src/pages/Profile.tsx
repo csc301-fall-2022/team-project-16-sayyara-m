e8beb1ddaec7fa42c4891d80b6acaa47150c456e
Ahsan Saeed <ahsanm.saeed@mail.utoronto.ca>
2022-11-01 20:10:21 -0400

    Divided profile page into seperate components

--- frontend/src/pages/Profile.tsx
+++ frontend/src/pages/Profile.tsx
@@ -5,6 +5,122 @@ import React, { useState } from "react";
 
 const Profile = () => {
 
+    const defaultProfilePage = () => {
+        return (
+            <div className='mx-8'>
+                <div className='flex justify-center my-8'>
+                    <AccountCircle fontSize="large" />
+                </div>
+                <div className='flex justify-center my-8'>
+                    { /* TODO: Fetch firstName and lastName of user from server */ }
+                    <label className='text-3xl font-bold'>firstName lastName</label>
+                </div>
+                <div className='flex justify-center my-8'>
+                    { /* TODO: Fetch username of user from server */ }
+                    <label className='text-1xl font-bold'>username</label>
+                </div>
+                <div className='flex justify-center mt-8'>
+                    <PhoneIcon className="mx-1" />
+                    { /* TODO: Fetch phoneNumber of user from server */ }
+                    <label className='text-1m mx-1'>123-456-789</label>
+                </div>
+                <div className='flex justify-center mt-4 mb-8'>
+                    <EmailIcon className="mx-1" />
+                    { /* TODO: Fetch email of user from server */ }
+                    <label className='text-1m mx-1'>someone@example.com</label>
+                </div>
+                <div className='flex justify-evenly my-8'>
+                    <button className="transition duration-100 ease-in-out w-34 bg-blue-500 hover:bg-blue-700 text-white
+                    font-semibold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button" onClick={() => setChangingPassword(true)}>
+                        Change Password
+                    </button>
+                    <button className="transition duration-100 ease-in-out w-32 bg-blue-500 hover:bg-blue-700 text-white
+                    font-semibold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button" onClick={() => setEditingProfile(true)}>
+                        Edit Profile
+                    </button>
+                </div>
+            </div>
+        )
+    }
+
+    const editProfilePage = () => {
+        return (
+            <div className="mx-8">
+                <div className='grid grid-cols-2 grid-rows-12 gap-1 mt-6 mb-6'>
+                    <label className='mb-[2px] font-semibold'>First Name</label>
+                    <label className='ml-2 font-semibold'>Last Name</label>
+                    <div className='mr-2'>
+                        <input className="shadow-sm appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight
+                        focus:outline-blue-500 focus:shadow-outline" type="text" placeholder="First Name" defaultValue="firstName"/>
+                    </div>
+                    <div className='ml-2'>
+                        <input className="shadow-sm appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight
+                        focus:outline-blue-500 focus:shadow-outline" type="text" placeholder="Last Name" defaultValue="lastName"/>
+                    </div>
+                    <label className='col-span-2 mt-6 font-semibold'>
+                        Email Address
+                    </label>
+                    <input className="col-span-2 shadow-sm appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight
+                    focus:outline-blue-500 focus:shadow-outline" type="text" placeholder="Email Address" defaultValue="someone@example.com"/>
+                    <label className='col-span-2 mt-6 font-semibold'>
+                        Phone Number
+                    </label>
+                    <input className="col-span-2 shadow-sm appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight
+                    focus:outline-blue-500 focus:shadow-outline" type="text" placeholder="Phone Number" defaultValue="123-456-789"/>
+                    <label className='col-span-2 mt-6 font-semibold'>
+                        Username
+                    </label>
+                    <input className="col-span-2 shadow-sm appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight
+                    focus:outline-blue-500 focus:shadow-outline" type="text" placeholder="Username" defaultValue="username"/>
+                </div>
+                <div className='flex justify-evenly my-8'>
+                    <button className="transition duration-100 ease-in-out w-32 bg-white hover:bg-gray-100 text-black
+                    font-semibold py-2 px-4 rounded border border-black" type="button" onClick={() => setEditingProfile(false)}>
+                        Cancel
+                    </button>
+                    <button className="transition duration-100 ease-in-out w-32 bg-blue-500 hover:bg-blue-700 text-white
+                    font-semibold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button" onClick={() => setEditingProfile(false)}>
+                        Save
+                    </button>
+                </div>
+            </div>
+        )
+    }
+
+    const changePasswordPage = () => {
+        return (
+            <div className="mx-8">
+                <div className='grid grid-cols-2 grid-rows-12 gap-1 mt-6 mb-6'>
+                    <label className='col-span-2 mt-6 font-semibold'>
+                        Old Password
+                    </label>
+                    <input className="col-span-2 shadow-sm appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight
+                    focus:outline-blue-500 focus:shadow-outline" type="password" placeholder="************"/>
+                    <label className='col-span-2 mt-6 font-semibold'>
+                        New Password
+                    </label>
+                    <input className="col-span-2 shadow-sm appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight
+                    focus:outline-blue-500 focus:shadow-outline" type="password" placeholder="************"/>
+                    <label className='col-span-2 mt-6 font-semibold'>
+                        Confirm Password
+                    </label>
+                    <input className="col-span-2 shadow-sm appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight
+                    focus:outline-blue-500 focus:shadow-outline" type="password" placeholder="************"/>
+                </div>
+                <div className='flex justify-evenly my-8'>
+                    <button className="transition duration-100 ease-in-out w-32 bg-white hover:bg-gray-100 text-black
+                    font-semibold py-2 px-4 rounded border border-black" type="button" onClick={() => setChangingPassword(false)}>
+                        Cancel
+                    </button>
+                    <button className="transition duration-100 ease-in-out w-32 bg-blue-500 hover:bg-blue-700 text-white
+                    font-semibold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button" onClick={() => setChangingPassword(false)}>
+                        Save
+                    </button>
+                </div>
+            </div>
+        )
+    }
+
     const [isEditingProfile, setEditingProfile] = useState<boolean>(false)
     const [isChangingPassword, setChangingPassword] = useState<boolean>(false)
 
@@ -13,109 +129,11 @@ const Profile = () => {
             <div className='flex flex-wrap h-full max-w-md min-w-[330px] w-full'>
                 <div className='w-full h-min border-2 border-gray-300 rounded-lg shadow-lg bg-white'>
                     {!isEditingProfile && !isChangingPassword ?
-                        <div className='mx-8'>
-                            <div className='flex justify-center my-8'>
-                                <AccountCircle fontSize="large" />
-                            </div>
-                            <div className='flex justify-center my-8'>
-                                { /* TODO: Fetch firstName and lastName of user from server */ }
-                                <label className='text-3xl font-bold'>firstName lastName</label>
-                            </div>
-                            <div className='flex justify-center my-8'>
-                                { /* TODO: Fetch username of user from server */ }
-                                <label className='text-1xl font-bold'>username</label>
-                            </div>
-                            <div className='flex justify-center mt-8'>
-                                <PhoneIcon className="mx-1" />
-                                { /* TODO: Fetch phoneNumber of user from server */ }
-                                <label className='text-1m mx-1'>123-456-789</label>
-                            </div>
-                            <div className='flex justify-center mt-4 mb-8'>
-                                <EmailIcon className="mx-1" />
-                                { /* TODO: Fetch email of user from server */ }
-                                <label className='text-1m mx-1'>someone@example.com</label>
-                            </div>
-                            <div className='flex justify-evenly my-8'>
-                                <button className="transition duration-100 ease-in-out w-34 bg-blue-500 hover:bg-blue-700 text-white
-                                font-semibold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button" onClick={() => setChangingPassword(true)}>
-                                    Change Password
-                                </button>
-                                <button className="transition duration-100 ease-in-out w-32 bg-blue-500 hover:bg-blue-700 text-white
-                                font-semibold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button" onClick={() => setEditingProfile(true)}>
-                                    Edit Profile
-                                </button>
-                            </div>
-                        </div>
+                        defaultProfilePage()
                         : !isChangingPassword ?
-                        <div className="mx-8">
-                            <div className='grid grid-cols-2 grid-rows-12 gap-1 mt-6 mb-6'>
-                                <label className='mb-[2px] font-semibold'>First Name</label>
-                                <label className='ml-2 font-semibold'>Last Name</label>
-                                <div className='mr-2'>
-                                    <input className="shadow-sm appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight
-                                    focus:outline-blue-500 focus:shadow-outline" type="text" placeholder="First Name" defaultValue="firstName"/>
-                                </div>
-                                <div className='ml-2'>
-                                    <input className="shadow-sm appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight
-                                    focus:outline-blue-500 focus:shadow-outline" type="text" placeholder="Last Name" defaultValue="lastName"/>
-                                </div>
-                                <label className='col-span-2 mt-6 font-semibold'>
-                                    Email Address
-                                </label>
-                                <input className="col-span-2 shadow-sm appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight
-                                focus:outline-blue-500 focus:shadow-outline" type="text" placeholder="Email Address" defaultValue="someone@example.com"/>
-                                <label className='col-span-2 mt-6 font-semibold'>
-                                    Phone Number
-                                </label>
-                                <input className="col-span-2 shadow-sm appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight
-                                focus:outline-blue-500 focus:shadow-outline" type="text" placeholder="Phone Number" defaultValue="123-456-789"/>
-                                <label className='col-span-2 mt-6 font-semibold'>
-                                    Username
-                                </label>
-                                <input className="col-span-2 shadow-sm appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight
-                                focus:outline-blue-500 focus:shadow-outline" type="text" placeholder="Username" defaultValue="username"/>
-                            </div>
-                            <div className='flex justify-evenly my-8'>
-                                <button className="transition duration-100 ease-in-out w-32 bg-white hover:bg-gray-100 text-black
-                                font-semibold py-2 px-4 rounded border border-black" type="button" onClick={() => setEditingProfile(false)}>
-                                    Cancel
-                                </button>
-                                <button className="transition duration-100 ease-in-out w-32 bg-blue-500 hover:bg-blue-700 text-white
-                                font-semibold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button" onClick={() => setEditingProfile(false)}>
-                                    Save
-                                </button>
-                            </div>
-                        </div>
+                        editProfilePage()
                         :
-                        <div className="mx-8">
-                            <div className='grid grid-cols-2 grid-rows-12 gap-1 mt-6 mb-6'>
-                                <label className='col-span-2 mt-6 font-semibold'>
-                                    Old Password
-                                </label>
-                                <input className="col-span-2 shadow-sm appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight
-                                focus:outline-blue-500 focus:shadow-outline" type="password" placeholder="************"/>
-                                <label className='col-span-2 mt-6 font-semibold'>
-                                    New Password
-                                </label>
-                                <input className="col-span-2 shadow-sm appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight
-                                focus:outline-blue-500 focus:shadow-outline" type="password" placeholder="************"/>
-                                <label className='col-span-2 mt-6 font-semibold'>
-                                    Confirm Password
-                                </label>
-                                <input className="col-span-2 shadow-sm appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight
-                                focus:outline-blue-500 focus:shadow-outline" type="password" placeholder="************"/>
-                            </div>
-                            <div className='flex justify-evenly my-8'>
-                                <button className="transition duration-100 ease-in-out w-32 bg-white hover:bg-gray-100 text-black
-                                font-semibold py-2 px-4 rounded border border-black" type="button" onClick={() => setChangingPassword(false)}>
-                                    Cancel
-                                </button>
-                                <button className="transition duration-100 ease-in-out w-32 bg-blue-500 hover:bg-blue-700 text-white
-                                font-semibold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button" onClick={() => setChangingPassword(false)}>
-                                    Save
-                                </button>
-                            </div>
-                        </div>
+                        changePasswordPage()
                     }
                 </div>
             </div>
