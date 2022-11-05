7fab19d6053d41cad853db765a79c052857ed323
Uthman Mohamed <uthman.mohamed@mail.utoronto.ca>
2022-11-05 19:59:08 -0400

    Add authentication filter with JWT tokens

--- backend/src/main/java/com/backend/spring/user/security/WebSecurityConfig.java
+++ backend/src/main/java/com/backend/spring/user/security/WebSecurityConfig.java
@@ -1,38 +1,52 @@
 package com.backend.spring.user.security;
 
+import com.backend.spring.user.security.filters.SimpleAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
-import static com.backend.spring.user.role.RoleEnum.SHOP_OWNER;
-import static com.backend.spring.user.role.RoleEnum.VEHICLE_OWNER;
-import static org.springframework.http.HttpMethod.POST;
 import static org.springframework.security.config.Customizer.withDefaults;
 import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 
+@Configuration
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class WebSecurityConfig {
     private final AppUserAuthenticationProvider authenticationProvider;
 
+    private final AuthenticationConfiguration authenticationConfiguration;
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        SimpleAuthenticationFilter authenticationFilter = new SimpleAuthenticationFilter(authenticationConfiguration.getAuthenticationManager());
+        authenticationFilter.setFilterProcessesUrl("/api/login");
+
         AuthenticationManagerBuilder authenticationManagerBuilder = http.getSharedObject(AuthenticationManagerBuilder.class);
         authenticationManagerBuilder.authenticationProvider(authenticationProvider);
         http.csrf().disable();
-        http.authorizeRequests().antMatchers(POST, "/api/**").permitAll();
-        http.authorizeRequests()
-                .anyRequest()
-                .hasAnyRole(SHOP_OWNER.getValue(), VEHICLE_OWNER.getValue())
-                .and()
-                .httpBasic(withDefaults())
-                .sessionManagement()
-                .sessionCreationPolicy(STATELESS);
+        http.authorizeRequests().anyRequest().permitAll();
+        http.httpBasic(withDefaults());
+
+        http.sessionManagement().sessionCreationPolicy(STATELESS);
+
+        http.addFilter(authenticationFilter);
+
+        // TODO: Allow specific route access based on role later on
+//        http.authorizeRequests().antMatchers(GET, "/api/**").permitAll();
+//        http.authorizeRequests()
+//                .anyRequest()
+//                .hasAnyRole(SHOP_OWNER.getValue(), VEHICLE_OWNER.getValue())
+//                .and()
+//                .httpBasic(withDefaults())
+//                .sessionManagement()
+//                .sessionCreationPolicy(STATELESS);
 
         return http.build();
     }
