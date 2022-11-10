ecb10d3404e264aa63847c2c2cfd0879dae42475
Uthman Mohamed <uthman.mohamed@mail.utoronto.ca>
2022-11-10 15:17:18 -0500

    Add GET /api/shopOwner
    - Added InvalidAuthorizationException to throw HTTP 403

--- backend/src/main/java/com/backend/spring/user/shopowner/ShopOwnerController.java
+++ backend/src/main/java/com/backend/spring/user/shopowner/ShopOwnerController.java
@@ -3,11 +3,15 @@ package com.backend.spring.user.shopowner;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
 import static org.springframework.http.HttpStatus.OK;
 import static org.springframework.http.HttpStatus.UNAUTHORIZED;
 
@@ -24,5 +28,11 @@ public class ShopOwnerController {
             return new ResponseEntity<>(OK);
         }
         return new ResponseEntity<>(UNAUTHORIZED);
+    }
+
+    @GetMapping
+    public ResponseEntity<ShopOwner> getShopOwner(HttpServletRequest request) {
+        String authorizationHeader = request.getHeader(AUTHORIZATION);
+        return ResponseEntity.ok().body(shopOwnerService.getShopOwner(authorizationHeader));
     }
 }
