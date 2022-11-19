01afb77f7fdc55dfb71b8beca25efa23c3ad8f86
Uthman Mohamed <uthman.mohamed@mail.utoronto.ca>
2022-11-18 23:58:21 -0500

    Add `GET /api/shops` to get all shops

--- backend/src/main/java/com/backend/spring/controllers/ShopController.java
+++ backend/src/main/java/com/backend/spring/controllers/ShopController.java
@@ -5,17 +5,18 @@ import com.backend.spring.services.ShopService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 import static org.springframework.http.HttpHeaders.AUTHORIZATION;
 
 @RestController
 @CrossOrigin
-@RequestMapping(path = "shop")
 public class ShopController {
     private final ShopService service;
 
@@ -24,9 +25,14 @@ public class ShopController {
         this.service = service;
     }
 
-    @PutMapping
+    @PutMapping("shop")
     public ResponseEntity<Shop> updateShop(@RequestBody Shop shop,
                                            @RequestHeader(AUTHORIZATION) String authorizationHeader) {
         return ResponseEntity.ok().body(service.updateShop(shop, authorizationHeader));
+    }
+
+    @GetMapping("shops")
+    public ResponseEntity<List<Shop>> getAllShops() {
+        return ResponseEntity.ok().body(service.getAllShops());
     }
 }
--- backend/src/main/java/com/backend/spring/services/ShopService.java
+++ backend/src/main/java/com/backend/spring/services/ShopService.java
@@ -2,20 +2,28 @@ package com.backend.spring.services;
 
 import com.backend.spring.entities.Shop;
 import com.backend.spring.exceptions.InvalidDataException;
+import com.backend.spring.repositories.ShopRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class ShopService {
     private final ShopOwnerRetriever shopOwnerRetriever;
 
+    private final ShopRepository shopRepository;
+
     @Transactional
     public Shop updateShop(Shop newShop, String authorization) throws InvalidDataException {
         Shop shop = shopOwnerRetriever.getShop(authorization);
         shop.update(newShop);
         return shop;
+    }
+
+    public List<Shop> getAllShops() {
+        return shopRepository.findAll();
     }
 }
