cf4de2246e772d8362796a4a8ef257df56dc6baf
Hamoon Zamiri <hamoon.zamiri@mail.utoronto.ca>
2022-11-20 17:30:11 -0500

    added quote popup form to shop details page

--- frontend/src/pages/ShopDetails.tsx
+++ frontend/src/pages/ShopDetails.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import CreateAppointmentForm from "src/components/CreateAppointmentForm";
+import CreateQuoteForm from "src/components/CreateQuoteForm";
 import { mShop as shop } from "../utilities/mockData";
 const generateServiceCards = () => {
     let services: JSX.Element[] = [];
@@ -12,6 +13,7 @@ const generateServiceCards = () => {
 }
 const ShopDetails = () => {
     const [apptPopup, setApptPopup] = useState(false);
+    const [quotePopup, setQuotePopup] = useState(false);
     return (
         <div className="">
             {/* Section to show the details of the shop */}
@@ -20,8 +22,13 @@ const ShopDetails = () => {
                     <CreateAppointmentForm setVisibility={setApptPopup}/>
                 </div>
             }
+            {quotePopup &&
+                <div className="w-full flex justify-center">
+                    <CreateQuoteForm setVisibility={setQuotePopup}/>
+                </div>
+            }
             {/* Div to set rest of page as hidden when form is selected */}
-            <div className={apptPopup ? "hidden" : ""}>
+            <div className={apptPopup || quotePopup ? "hidden" : ""}>
                 <section className="grid-cols-1 gap-3 grid p-4">
                     <h1 className="font-semibold text-2xl pt-2 text-blue-800 sm:text-3xl">About "{shop.name}"</h1>
                     <h3 className="font-semibold text-sm sm:text-lg">Located At: {shop.address.streetNumber} {shop.address.street}, {shop.address.city} {shop.address.province}</h3>
@@ -42,6 +49,7 @@ const ShopDetails = () => {
                         Book an Appointment
                     </button>
                     <button
+                        onClick={() => setQuotePopup(!quotePopup)}
                         className="whitespace-nowrap w-[170px] transition duration-100 ease-in-out bg-white hover:bg-gray-100
                         text-black font-semibold py-2 px-4 rounded border border-black">
                         Request a Quote
