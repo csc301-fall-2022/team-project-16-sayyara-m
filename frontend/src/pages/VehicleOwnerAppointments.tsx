3071959258b8f27a3552b255ef93d8e992f6bbf6
Ahsan Saeed <ahsanm.saeed@mail.utoronto.ca>
2022-11-24 21:02:54 -0500

    API calls for fetching appointments and quotes

--- frontend/src/pages/VehicleOwnerAppointments.tsx
+++ frontend/src/pages/VehicleOwnerAppointments.tsx
@@ -1,11 +1,11 @@
 import { DataGrid, GridColDef, GridRowParams, MuiEvent } from "@mui/x-data-grid";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { VehicleOwner } from "../utilities/interfaces";
+import { API_ROOT } from "../utilities/constants";
+import { APIError, Appointment, VehicleOwner } from "../utilities/interfaces";
 import { mAppointment } from "../utilities/mockData";
 
 const columns: GridColDef[] = [
-    { field: 'id', headerName: 'ID', width: 70 },
     { field: 'firstName', headerName: 'First name', width: 200},
     { field: 'lastName', headerName: 'Last name', width: 200 },
     { field: 'date', headerName: 'Date', width: 150 },
@@ -19,8 +19,9 @@ const columns: GridColDef[] = [
     { field: 'serviceType', headerName: 'Service Type', width: 160 },
   ];
 
-const generateApptRows = () => {
+const generateApptRows = (appointments: Appointment[]) => {
     let rows = [];
+    let appointmentRows = []
     const vehicleOwner: VehicleOwner = mAppointment.vehicleOwner;
     for(let i = 0; i < 20; i++){
         rows.push({
@@ -34,11 +35,51 @@ const generateApptRows = () => {
             serviceType: "Oil Change"
         });
     }
-    return rows;
+    for (var appointment of appointments) {
+        const apptVehicleOwner: VehicleOwner = appointment.vehicleOwner;
+        appointmentRows.push({
+            id: appointment.id,
+            firstName: apptVehicleOwner.firstName,
+            lastName: apptVehicleOwner.lastName,
+            date: new Date(appointment.startTime).toISOString().substring(0, 10),
+            startDate: new Date(appointment.startTime).toLocaleTimeString(),
+            endDate: new Date(appointment.endTime).toLocaleTimeString(),
+            Duration: appointment.duration,
+            serviceType: appointment.service.name
+        })
+    }
+    // Return rows to test with mock data
+    return appointmentRows;
 }
 
 const VehicleOwnerAppointments = () => {
     let navigate = useNavigate();
+    const [appointments, setAppointments] = useState<Appointment[]>([])
+
+    useEffect(() => {
+        const getData = async () => {
+            let ids = [1, 2, 3]
+            let newAppointments: Appointment[] = []
+            for (var id of ids) {
+                const res = await fetch(API_ROOT + "/appointments/" + id, {
+                    method: "GET",
+                })
+    
+                if (res.ok) {
+                    const data: Appointment = await res.json();
+                    newAppointments.push(data)   
+                }
+    
+                else {
+                    const data: APIError = await res.json();
+                    console.log(data.message);
+                }
+            }
+            setAppointments(newAppointments)
+        }
+        getData();
+    }, [])
+
     const handleRowClick = (params: GridRowParams, event: MuiEvent<React.MouseEvent<HTMLElement, MouseEvent>>) => {
         navigate(`/appointments/${params.id}`);
     }
@@ -47,7 +88,7 @@ const VehicleOwnerAppointments = () => {
         <div className="h-[650px] w-full">
             <h1 className="flex justify-center font-semibold text-blue-500 sm:text-3xl py-4">Upcoming Appointments</h1>
             <DataGrid
-                rows={generateApptRows()}
+                rows={generateApptRows(appointments)}
                 columns={columns}
                 pageSize={10}
                 rowsPerPageOptions={[10]}
--- frontend/src/pages/VehicleOwnerQuotes.tsx
+++ frontend/src/pages/VehicleOwnerQuotes.tsx
@@ -1,11 +1,11 @@
 import { DataGrid, GridColDef, GridRowParams, MuiEvent } from "@mui/x-data-grid";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { VehicleOwner } from "../utilities/interfaces";
+import { API_ROOT } from "../utilities/constants";
+import { APIError, Quote, VehicleOwner } from "../utilities/interfaces";
 import { mQuote } from "../utilities/mockData";
 
 const columns: GridColDef[] = [
-    { field: 'id', headerName: 'ID', width: 70 },
     { field: 'firstName', headerName: 'First name', width: 200},
     { field: 'lastName', headerName: 'Last name', width: 200 },
     { field: 'price', headerName: 'Price', width: 100 },
@@ -13,8 +13,9 @@ const columns: GridColDef[] = [
     { field: 'serviceType', headerName: 'Service Type', width: 160 },
   ];
 
-  const generateQuoteRows = () => {
+  const generateQuoteRows = (quotes: Quote[]) => {
     let rows = [];
+    let quoteRows = []
     const vehicleOwner: VehicleOwner = mQuote.vehicleOwner;
     for(let i = 0; i < 20; i++){
         rows.push({
@@ -26,11 +27,52 @@ const columns: GridColDef[] = [
             serviceType: mQuote.serviceType
         });
     }
-    return rows;
+    for (var quote of quotes) {
+        const quoteVehicleOwner: VehicleOwner = quote.vehicleOwner;
+        quoteRows.push({
+            id: quote.id,
+            firstName: quoteVehicleOwner.firstName,
+            lastName: quoteVehicleOwner.lastName,
+            price: `$${quote.price}`,
+            expiryTime: new Date(quote.expiryTime).toLocaleString(),
+            serviceType: quote.service.name
+        })
+    }
+    // Return rows to test with mock data
+    return quoteRows;
 }
 
 const VehicleOwnerQuotes = () => {
     let navigate = useNavigate();
+    const [quotes, setQuotes] = useState<Quote[]>([])
+
+    useEffect(() => {
+        const getData = async () => {
+            let ids = [1, 2, 3]
+            let newQuotes: Quote[] = []
+            for (var id of ids) {
+                console.log(API_ROOT + "/quotes/" + id)
+                const res = await fetch(API_ROOT + "/quotes/" + id, {
+                    method: "GET",
+                })
+    
+                if (res.ok) {
+                    const data: Quote = await res.json();
+                    console.log(data)
+                    newQuotes.push(data) 
+    
+                }
+    
+                else {
+                    const data: APIError = await res.json();
+                    console.log(data.message);
+                }
+            }
+            setQuotes(newQuotes)
+        }
+        getData();
+    }, [])
+
     const handleRowClick = (params: GridRowParams, event: MuiEvent<React.MouseEvent<HTMLElement, MouseEvent>>) => {
         navigate(`/quotes/${params.id}`);
     }
@@ -39,7 +81,7 @@ const VehicleOwnerQuotes = () => {
         <div className="h-[650px] w-full">
             <h1 className="flex justify-center font-semibold text-blue-500 sm:text-3xl py-4">Your Quotes</h1>
             <DataGrid
-                rows={generateQuoteRows()}
+                rows={generateQuoteRows(quotes)}
                 columns={columns}
                 pageSize={10}
                 rowsPerPageOptions={[10]}
