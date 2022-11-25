00dd961d080edfb294bae81d59b5d152853ea7a9
Uthman Mohamed <uthman.mohamed@mail.utoronto.ca>
2022-11-25 00:59:28 -0500

    Add VehicleOwnerController and VehicleOwnerService
    - Add `GET /api/vehicleOwner/{vehicleOwnerId}/quotes` and
    `POST /api/vehicleOwner/quotes`
    - Add these endpoints to api docs

--- backend/src/main/java/com/backend/spring/controllers/QuoteController.java
+++ backend/src/main/java/com/backend/spring/controllers/QuoteController.java
@@ -3,6 +3,7 @@ package com.backend.spring.controllers;
 import com.backend.spring.entities.Quote;
 import com.backend.spring.exceptions.InvalidDataException;
 import com.backend.spring.services.QuoteService;
+import com.backend.spring.services.VehicleOwnerService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -10,7 +11,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,6 +27,8 @@ import static org.springframework.http.HttpHeaders.AUTHORIZATION;
 public class QuoteController {
     private final QuoteService quoteService;
 
+    private final VehicleOwnerService vehicleOwnerService;
+
     @GetMapping
     public ResponseEntity<List<Quote>> getAllQuotes(@RequestHeader(AUTHORIZATION) String authorizationHeader) {
         return ResponseEntity.ok(quoteService.getAllQuotes(authorizationHeader));
@@ -36,22 +39,16 @@ public class QuoteController {
         return ResponseEntity.ok(quoteService.getQuote(quoteId));
     }
 
-    @DeleteMapping(path = "{quoteId}")
-    public void deleteQuote(@PathVariable long quoteId) {
-        quoteService.deleteQuote(quoteId);
+
+    @DeleteMapping(path = "{quote_id}")
+    public void deleteQuote(@PathVariable("quote_id") long id) {
+        quoteService.deleteQuote(id);
     }
 
-    @PatchMapping(path = "{quoteId}")
-    public ResponseEntity<Quote> updateQuote(@PathVariable long quoteId,
-                                             @RequestBody QuoteInfo quoteInfo) {
-        if (quoteInfo.price == null && quoteInfo.status == null)
-            return ResponseEntity.noContent().build();
-        if (quoteInfo.price == null)
-            return ResponseEntity.ok(quoteService.updateQuoteStatus(quoteId, quoteInfo.status));
-        if (quoteInfo.status == null)
-            return ResponseEntity.ok(quoteService.updateQuotePrice(quoteId, quoteInfo.price));
-        throw new InvalidDataException("Invalid data. Must provide either price or status.");
+    @PutMapping(path = "{quote_id}")
+    public void updateQuote(@PathVariable("quote_id") Long id,
+                            @RequestParam(required = false) Double price,
+                            @RequestParam(required = false) LocalDateTime expiryDate) {
+        quoteService.updateQuote(id, price, expiryDate);
     }
-
-    private record QuoteInfo(String status, Double price){}
 }
--- backend/src/main/java/com/backend/spring/services/QuoteService.java
+++ backend/src/main/java/com/backend/spring/services/QuoteService.java
@@ -7,6 +7,8 @@ import com.backend.spring.exceptions.DataNotFoundException;
 import com.backend.spring.exceptions.ViolatedConstraintException;
 import com.backend.spring.exceptions.ViolatedConstraintException;
 import com.backend.spring.repositories.QuoteRepository;
+import com.backend.spring.repositories.ShopRepository;
+import com.backend.spring.repositories.VehicleOwnerRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -25,7 +27,7 @@ public class QuoteService {
     }
 
     public Quote getQuote(long id) {
-        return quoteRepository.findById(id).orElseThrow(() -> new DataNotFoundException("Quote with id " + id + " not found."));
+        return repository.findById(id).orElseThrow(() -> new DataNotFoundException("Quote with id " + id + " not found."));
     }
 
     public void deleteQuote(long id) {
