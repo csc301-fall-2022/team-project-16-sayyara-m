2c28393d9ad2b2f092f03a431b5a914ab2835829
Hamoon Zamiri <hamoon.zamiri@mail.utoronto.ca>
2022-11-25 21:06:18 -0500

    uses api now to fetch appointments

--- frontend/src/pages/Appointments.tsx
+++ frontend/src/pages/Appointments.tsx
@@ -1,21 +1,21 @@
 import React from "react";
 import { DataGrid, GridColDef, GridRowParams, MuiEvent } from "@mui/x-data-grid";
-import { mAppointment as appt } from "src/utilities/mockData";
-import { VehicleOwner } from "src/utilities/interfaces";
+import { Appointment, VehicleOwner } from "src/utilities/interfaces";
 import { useNavigate } from "react-router-dom";
+import { useGetAllAppointments } from "src/utilities/hooks/api/useGetAllAppointments";
 //plan is to use MUI to create a detailed data table of all the appointments for this user
 
 const columns: GridColDef[] = [
     { field: 'id', headerName: 'ID', width: 70 },
     { field: 'firstName', headerName: 'First name', width: 200},
     { field: 'lastName', headerName: 'Last name', width: 200 },
-    { field: 'date', headerName: 'Date', width: 150 },
-    { field: 'startTime', headerName: 'Start Time', width: 100 },
-    { field: 'endTime', headerName: 'End Time', width: 100 },
+    { field: 'date', headerName: 'Date', width: 200 },
+    { field: 'startTime', headerName: 'Start Time', width: 200 },
+    { field: 'endTime', headerName: 'End Time', width: 200 },
     {
       field: 'Duration',
       headerName: 'Duration',
-      width: 90,
+      width: 150,
     },
     { field: 'serviceType', headerName: 'Service Type', width: 160 },
     // {
@@ -29,12 +29,12 @@ const columns: GridColDef[] = [
     // },
   ];
 
-const generateApptRows = () => {
+const generateApptRows = (appointments: Appointment[]) => {
     let apptRows = [];
-    const vehicleOwner: VehicleOwner = appt.vehicleOwner;
-    for(let i = 0; i < 20; i++){
+    for(let appt of appointments){
+        const vehicleOwner: VehicleOwner = appt.vehicleOwner;
         apptRows.push({
-            id: (appt.id + i),
+            id: (appt.id),
             firstName: vehicleOwner.firstName,
             lastName: vehicleOwner.lastName,
             date: new Date(appt.startTime).toISOString().substring(0, 10),
@@ -48,6 +48,7 @@ const generateApptRows = () => {
 }
 const Appointments = () => {
     let navigate = useNavigate();
+    const { appointments } = useGetAllAppointments();
     const handleRowClick = (params: GridRowParams, event: MuiEvent<React.MouseEvent<HTMLElement, MouseEvent>>) => {
         console.log("row clicked");
         navigate(`/appointments/${params.id}`);
@@ -56,7 +57,7 @@ const Appointments = () => {
         <div className="h-[650px] w-full">
             <h1 className="flex justify-center font-semibold text-blue-500 sm:text-3xl py-4">Upcoming Appointments</h1>
             <DataGrid
-                rows={generateApptRows()}
+                rows={generateApptRows(appointments)}
                 columns={columns}
                 pageSize={10}
                 rowsPerPageOptions={[10]}
