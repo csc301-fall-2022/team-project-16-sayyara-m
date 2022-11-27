e507b2657d61c76fe98866a1841e6af8207fe537
Uthman Mohamed <uthman.mohamed@mail.utoronto.ca>
2022-11-27 15:54:51 -0500

    Fix createQuote bug while VehicleOwner exists
    Needed to create 2 paths where if the vehicle owner already exists,
    just add the quote to the vehicle owner and save the quote. Otherwise,
    if the vehicle owner doesn't exist, set the quote to the vehicle owner
    and save the VehicleOwner instead of the quote

--- backend/src/main/java/com/backend/spring/services/VehicleOwnerService.java
+++ backend/src/main/java/com/backend/spring/services/VehicleOwnerService.java
@@ -4,13 +4,16 @@ import com.backend.spring.entities.Quote;
 import com.backend.spring.entities.Shop;
 import com.backend.spring.entities.VehicleOwner;
 import com.backend.spring.exceptions.DataNotFoundException;
+import com.backend.spring.repositories.QuoteRepository;
 import com.backend.spring.repositories.ShopRepository;
 import com.backend.spring.repositories.VehicleOwnerRepository;
 import com.backend.spring.validators.QuoteValidator;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -20,6 +23,8 @@ public class VehicleOwnerService {
     private final VehicleOwnerSaveHelper vehicleOwnerSaveHelper;
 
     private final ShopRepository shopRepository;
+
+    private final QuoteRepository quoteRepository;
 
     public List<Quote> getAllQuotes(long id) {
         return vehicleOwnerRepository
@@ -34,27 +39,39 @@ public class VehicleOwnerService {
         Shop shop = shopRepository.findById(quote.getShopId())
                 .orElseThrow(() -> new DataNotFoundException("Shop with id " + quote.getShopId() + " not found."));
 
-        if (serviceInShopIsNotNull(quote.getServiceName(), shop))
-            throw new DataNotFoundException("Shop already has a default price for the service " + quote.getServiceName() + ". Consider booking an appointment instead.");
+        checkServiceInShopIsNotNull(quote.getServiceName(), shop);
 
         quote.setShop(shop);
         quote.setService(new com.backend.spring.entities.Service(quote.getServiceName()));
 
-        VehicleOwner vehicleOwner = vehicleOwnerRepository
-                .findByEmail(quote.getVehicleOwner().getEmail())
-                .orElseGet(quote::getVehicleOwner);
-
-        vehicleOwner.getQuotes().add(quote);
-        vehicleOwner = vehicleOwnerSaveHelper.save(vehicleOwner);
+        Optional<VehicleOwner> savedVehicleOwner = vehicleOwnerRepository
+                .findByEmail(quote.getVehicleOwner().getEmail());
+        VehicleOwner vehicleOwner;
+        if (savedVehicleOwner.isPresent()) {
+            vehicleOwner = savedVehicleOwner.get();
+            addQuoteToVehicleOwner(quote, vehicleOwner);
+        } else {
+            vehicleOwner = quote.getVehicleOwner();
+            vehicleOwner.getQuotes().add(quote);
+            vehicleOwner = vehicleOwnerSaveHelper.save(vehicleOwner);
+        }
 
         return vehicleOwner
                 .getQuotes()
                 .get(vehicleOwner.getQuotes().size() - 1);
     }
 
-    private boolean serviceInShopIsNotNull(String serviceName, Shop shop) {
-        return shop.getServices()
+    @Transactional
+    void addQuoteToVehicleOwner(Quote quote, VehicleOwner vehicleOwner) {
+        vehicleOwner.getQuotes().add(quote);
+        quote.setVehicleOwner(vehicleOwner);
+        quoteRepository.save(quote);
+    }
+
+    private void checkServiceInShopIsNotNull(String serviceName, Shop shop) {
+        if (shop.getServices()
                 .stream()
-                .anyMatch(shopService -> shopService.getName().equals(serviceName) && shopService.getDefaultPrice() != null);
+                .anyMatch(shopService -> shopService.getName().equals(serviceName) && shopService.getDefaultPrice() != null))
+            throw new DataNotFoundException("Shop already has a default price for the service " + serviceName + ". Consider booking an appointment instead.");
     }
 }
