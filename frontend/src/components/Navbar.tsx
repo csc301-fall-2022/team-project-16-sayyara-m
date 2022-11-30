dc5b54746caa16c073d1b5176f9259527df170fc
Hamoon Zamiri <hamoon.zamiri@mail.utoronto.ca>
2022-11-30 11:33:21 -0500

    fixed detection bug on navbar

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -3,7 +3,7 @@ import {Link} from "react-router-dom";
 import {Disclosure} from "@headlessui/react"
 import ProfileDropdown from "./ProfileDropdown";
 import MenuIcon from '@mui/icons-material/Menu';
-import useAuth from "../utilities/hooks/useAuth";
+import { useCookies } from "react-cookie";
 
 //hidden on web version
 //sm:hidden on mobile version
@@ -31,17 +31,19 @@ const WebNavMap = () => {
     return (
         <div className="hidden sm:flex sm:text-lg md:text-xl">
             {navigation.map(item => (
-                <button className="transition duration-100 ease-in-out  bg-gray-10 hover:bg-blue-500 \
+                <Link to={item.to}>
+                    <button className="transition duration-100 ease-in-out  bg-gray-10 hover:bg-blue-500 \
         font-semibold py-2 px-4 rounded focus:outline-none focus:shadow-outline ml-2" type="button">
-                    <Link to={item.to}>{item.name}</Link>
-                </button>
+                        {item.name}
+                    </button>
+                </Link>
             ))}
         </div>
 
     )
 };
 const Navbar = () => {
-    const { auth } = useAuth();
+    const [cookies] = useCookies(['refresh_token']);
     return (
         <Disclosure as="nav" className="bg-gray-200 w-full px-4 sm:px-6 \
         pt-6 pb-5 md:px-8">
@@ -60,7 +62,7 @@ const Navbar = () => {
                 <WebNavMap/>
 
                 <div className="flex w-2/5 text-right justify-end items-center text-sm sm:ml-auto sm:text-xl">
-                    {auth ? <ProfileDropdown/>
+                    {cookies.refresh_token ? <ProfileDropdown/>
                      :<div>
                         <Link className="mx-2" to="/login">Login</Link>
                         <Link className="mx-2 text-ellipsis" to="/SignUp">Sign Up</Link>
