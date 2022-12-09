5f4fc8d8a06457561c9fa62dcaf6977219e8e4b0
Uthman Mohamed <uthman.mohamed@mail.utoronto.ca>
2022-12-08 19:15:52 -0500

    Add documentation to ShopOwnerRetriever

--- backend/src/main/java/com/backend/spring/services/ShopOwnerRetriever.java
+++ backend/src/main/java/com/backend/spring/services/ShopOwnerRetriever.java
@@ -18,12 +18,27 @@ class ShopOwnerRetriever {
 
     private final ShopOwnerRepository shopOwnerRepository;
 
+    /**
+     * Parses the authentication header and retrieves the shop owner object from it.
+     *
+     * @param authorization Authorization header received from an HTTP request
+     * @return Shop owner object associated with the username in the authorization header
+     * @throws InvalidDataException If the authorization header is invalid
+     */
     ShopOwner getShopOwner(String authorization) throws InvalidDataException {
         String username = new AuthHeaderParser(authorization).getUsername();
 
-        return shopOwnerRepository.findByUsername(username).orElseThrow(() -> new DataNotFoundException("Shop Owner with username " + username + " not found"));
+        return shopOwnerRepository.findByUsername(username).orElseThrow(() ->
+                new DataNotFoundException("Shop Owner with username " + username + " not found"));
     }
 
+    /**
+     * Parses the authentication header and retrieves the shop object from it.
+     *
+     * @param authorization Authorization header received from an HTTP request
+     * @return Shop object associated with the username in the authorization header
+     * @throws InvalidDataException If the authorization header is invalid
+     */
     Shop getShop(String authorization) throws InvalidDataException {
         return getShopOwner(authorization).getShop();
     }
